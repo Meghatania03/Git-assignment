cb23894177229e95ea402e92d4f646022e8f050c
Meghatania03 <141716710+Meghatania03@users.noreply.github.com>
2025-08-21 19:34:25 +0600

    Added Grade.java with grading system

---
+++ Grade.java
@@ -0,0 +1,11 @@
+public class Grade {
+    public class Grade {
+    public String getGrade(int marks) {
+        if (marks >= 90) return "A";
+        else if (marks >= 75) return "B";
+        else if (marks >= 50) return "C";
+        else return "F";
+    }
+}
+
+}
